08c580b775de962dfbdf982eed685556dd5a279c
Petre <BPetre@luxoft.com>
2021-11-11 15:18:25 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,47 +1,32 @@
 import json
 from cloudctx import ClassCtx
 
-with open('demo_response_capic.json','r') as json_file:
+with open('demo_response_capic.json', 'r') as json_file:
     json_dict = json.load(json_file)
 
-# print(json_dict)
+
+def check_for_empty(string):
+    if string == "":
+        return "-"
+    else:
+        return string
 
 
-if __name__=='__main__':
-
-    # for i in json_dict.keys():
-    #     if i == 'imdata':
-    #         hcloud_data = json_dict['imdata'][0]
-    #         # print(hcloud_data['hcloudCtx'])
+if __name__ == '__main__':
 
     atributes = json_dict['imdata'][0]['hcloudCtx']['attributes']
     for i in atributes.keys():
         if i == 'name':
-            if atributes[i] == "":
-                name = '-'
-            else:
-                name = atributes[i]
+            name = check_for_empty(atributes[i])
         elif i == 'tenantName':
-            if atributes[i] == "":
-                tenant_name = '-'
-            else:
-                tenant_name = atributes[i]
+            tenant_name = check_for_empty(atributes[i])
         elif i == 'description':
-            if atributes[i] == "":
-                description = '-'
-            else:
-                description = atributes[i]
+            description = check_for_empty(atributes[i])
         elif i == 'nameAlias':
-            if atributes[i] == "":
-                name_alias = '-'
-            else:
-                name_alias = atributes[i]
+            name_alias = check_for_empty(atributes[i])
         elif i == 'ctxProfileName':
-            if atributes[i] == "":
-                ctx_profile_name = '-'
-            else:
-                ctx_profile_name = atributes[i]
+            ctx_profile_name = check_for_empty(atributes[i])
 
-
-    MO = ClassCtx(name,tenant_name,description,name_alias,ctx_profile_name)
+    MO = ClassCtx(name, tenant_name, description,
+                  name_alias, ctx_profile_name)
     MO.display_info()
