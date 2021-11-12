4757c94f61c6d783b3f85bbd270d1052b830be46
Petre <BPetre@luxoft.com>
2021-11-12 12:25:26 +0200

    Added functionality to read all hcloud instances

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import json
 from cloudctx import ClassCtx
 
-with open('demo_response_capic.json', 'r') as json_file:
-    json_dict = json.load(json_file)
-
 
 def check_for_empty(string):
     if string == "":
@@ -14,29 +11,40 @@ def check_for_empty(string):
 
 if __name__ == '__main__':
 
-    atributes = json_dict['imdata'][0]['hcloudCtx']['attributes']
-    for i in atributes.keys():
-        if i == 'name':
-            name = check_for_empty(atributes[i])
-        elif i == 'tenantName':
-            tenant_name = check_for_empty(atributes[i])
-        elif i == 'description':
-            description = check_for_empty(atributes[i])
-        elif i == 'nameAlias':
-            name_alias = check_for_empty(atributes[i])
-        elif i == 'ctxProfileName':
-            ctx_profile_name = check_for_empty(atributes[i])
+    with open('demo_response_capic.json', 'r') as json_file:
+        json_dict = json.load(json_file)
 
-    atributes_health = json_dict['imdata'][0]['hcloudCtx']['children'][0]['healthInst']['attributes']
-    for i in atributes_health.keys():
-        if i == 'cur':
-            current_health = int(atributes_health[i])
-        elif i == 'maxSev':
-            max_sev = atributes_health[i]
+    nr_ctx = int(json_dict['totalCount'])
+    lista_instante = []
 
-    MO = ClassCtx(name, tenant_name, description,
-                  name_alias, ctx_profile_name,
-                  current_health, max_sev)
+    for element in range(nr_ctx):
+        atributes = json_dict['imdata'][element]['hcloudCtx']['attributes']
+        for i in atributes.keys():
+            if i == 'name':
+                name = check_for_empty(atributes[i])
+            elif i == 'tenantName':
+                tenant_name = check_for_empty(atributes[i])
+            elif i == 'description':
+                description = check_for_empty(atributes[i])
+            elif i == 'nameAlias':
+                name_alias = check_for_empty(atributes[i])
+            elif i == 'ctxProfileName':
+                ctx_profile_name = check_for_empty(atributes[i])
 
-    MO.display_info()
-    MO.health.display_health()
+        atributes_health = json_dict['imdata'][element]['hcloudCtx']['children'][0]['healthInst']['attributes']
+        for i in atributes_health.keys():
+            if i == 'cur':
+                current_health = int(atributes_health[i])
+            elif i == 'maxSev':
+                max_sev = atributes_health[i]
+
+        MO = ClassCtx(name, tenant_name, description,
+                      name_alias, ctx_profile_name,
+                      current_health, max_sev)
+        lista_instante.append(MO)
+
+    for index,i in enumerate(lista_instante):
+        print(f"hcloudCtx number {index}")
+        i.display_info()
+        i.health.display_health()
+        print()
